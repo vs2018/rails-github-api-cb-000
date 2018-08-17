a15999e8e6cb7cec8f4838748bd8a8a429aa301f
Vishal Sharma <vsh1819@gmail.com>
2018-08-17 07:08:44 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
     resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
    req.params['client_id'] = 9b3367fa1fe4dd9f4382
    req.params['client_secret'] = a2b1ef0558d7bc5b7d6e77d662f72262ecbf3e9f
-   req.params['grant_type'] = 'authorization_code'
    req.params['redirect_uri'] = "http://localhost:3000/auth"
    req.params['code'] = params[:code]
  end
