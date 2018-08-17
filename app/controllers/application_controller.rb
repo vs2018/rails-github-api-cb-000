9b236bc068825513268ec3d5699e6aae7f4d486e
Vishal Sharma <vsh1819@gmail.com>
2018-08-17 07:05:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=repo"
 
 
-    redirect_to "https://github.com/login/oauth/authorize?client_id=#{client_id}" unless logged_in?
+    redirect_to github_url unless logged_in?
   end
 
   def logged_in?
