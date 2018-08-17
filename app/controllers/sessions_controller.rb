1c4e1e15e51a1d0abea559aed8a36229b524b14b
Vishal Sharma <vsh1819@gmail.com>
2018-08-17 07:14:59 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
-      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+      req.body = { 'client_id': , 'client_secret': client_secret, 'code': code }
      req.headers['Accept'] = 'application/json'
  end
 
